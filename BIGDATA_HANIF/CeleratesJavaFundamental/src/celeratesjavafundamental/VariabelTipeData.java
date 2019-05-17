5834cc9b66ac808e5a2882008190adaae2092e06
Muhammad Hanif <haniframadan@gmail.com>
2019-05-17 14:51:10 +0700

    Push Folder Hanif

---
+++ BIGDATA_HANIF/CeleratesJavaFundamental/src/celeratesjavafundamental/VariabelTipeData.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package celeratesjavafundamental;
+
+/**
+ *
+ * @author Hanif
+ */
+public class VariabelTipeData {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Nama     : Muhammad Hanif \nNo. Telp : 0812345");
+        System.out.println(
+            (2+3)
+            + "\n"
+            + (2-5)
+            + "\n"
+            + (8/4)
+            + "\n"
+            + (89*5)
+            + "\n"
+            + (78%19)
+        );
+    }
+}
