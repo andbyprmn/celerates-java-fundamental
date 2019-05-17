2e5ae9157b6eeefb009fe82317c27f28f93c1124
Your Name <budi.juarto@gmail.com>
2019-05-17 14:45:19 +0700

    coba ya

---
+++ EAD_Budi/CeleratesJavaFundamental/src/celeratesjavafundamental/VariabelTipeData.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package celeratesjavafundamental;
+
+/**
+ *
+ * @author ACER
+ */
+public class VariabelTipeData {
+      public static void main(String[] args) {
+        //membuat variable
+        String nama;
+        String alamat;
+        String nomor;
+        
+        
+        //mengisi variable
+        nama = "budi";
+        alamat="rumah";
+        nomor = "081227324221";
+        
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("alamat : " + alamat);
+        System.out.println("nomor telp : " + nomor);
+ 
+    }
+}
