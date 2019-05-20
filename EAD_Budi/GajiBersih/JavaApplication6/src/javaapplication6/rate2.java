1c356cbc532e2ce7afed14bac3e273a3bb3e17e0
Your Name <budi.juarto@gmail.com>
2019-05-20 12:01:51 +0700

    pid

---
+++ EAD_Budi/GajiBersih/JavaApplication6/src/javaapplication6/rate2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author ACER
+ */
+public class rate2 {
+   public static void main(String[] args) {
+    int a = 30;
+    int b =  30;
+    int c = 60;
+    
+  System.out.println("Rata-ratanya: " + 
+   (a+b+c) / 3);
+    
+}
+}
