cb07220eee909ba2e760761ba12f35ff83662432
lisafitasari <fitasarilisa13@gmail.com>
2019-05-20 11:46:09 +0700

    Lisa Fita sari

---
+++ BIGDATA_Lisa/NilaiRata2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package celeratesjavafundamental;
+
+/**
+ *
+ * @author Lisa Fita Sari
+ */
+public class NilaiRata2 {
+    public static void main (String[] args) {            
+        int Average = (30 + 30 + 60) / 3;
+        System.out.println(Average);
+        int gajikotor = 4000000;
+        double potongan = gajikotor * 0.1;
+        double gajibersih = gajikotor - potongan;
+        System.out.println(gajibersih);     
+     }
+}
