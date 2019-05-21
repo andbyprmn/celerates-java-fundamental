4294388ca4ef25f07ed73f87a5f0ebbcee1726fc
Muhammad Ega S <simatupang.ega@gmail.com>
2019-05-21 13:32:18 +0700

    Update PerulanganJava.java

--- EAD_EGA/Latihan-3/PerulanganJava.java
+++ EAD_EGA/Latihan-3/PerulanganJava.java
@@ -29,5 +29,16 @@ public class PerulanganJava {
             sum+=i;
         }
         System.out.print("Total = " + sum + "\n\n");
+
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        for(int cetakArr : arr) {
+            System.out.println(cetakArr);
+        }
+
+        sum = 0;
+        for(i = 0; i<10; i++) {
+            sum+=arr[i];
+        }
+        System.out.print("Total = " + sum + "\n\n");
     }
 }
