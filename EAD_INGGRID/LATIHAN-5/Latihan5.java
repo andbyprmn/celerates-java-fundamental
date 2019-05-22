e4921fbd54fc40be1f7476be3d62031ba3330eb7
inggridmd <inggridmelanica@gmail.com>
2019-05-22 15:24:05 +0700

    latihan 5

--- EAD_INGGRID/LATIHAN-5/Latihan5.java
+++ EAD_INGGRID/LATIHAN-5/Latihan5.java
@@ -7,14 +7,11 @@ public class Latihan5 {
         PopulateObjek lingkaran = new PopulateObjek();
 
         Scanner input = new Scanner (System.in);
-        
-        int n;
-        int i;
+
         int a, b;
         int diameter;
         int jarijari;
         int tinggi;
-        
         
         System.out.println("PROGRAM PERHITUNGAN VOLUME BANGUN RUANG\n");
      
@@ -23,9 +20,10 @@ public class Latihan5 {
         System.out.println("Masukan tinggi = ");
         b = input.nextInt();
         
-        System.out.println("\n1. Volume Tabung ialah = " + lingkaran.voltabung(a, b));
-        System.out.println("2. Volume Kerucut ialah = " + lingkaran.volkerucut(a, b));
-        System.out.println("3. Volume Bola ialah = " + lingkaran.volbola(a));
+        System.out.println("\n1. Luas Lingkaran ialah = " + lingkaran.luasLingkaran(a));
+        System.out.println("2. Volume Tabung ialah = " + lingkaran.voltabung(a, b));
+        System.out.println("3. Volume Kerucut ialah = " + lingkaran.volkerucut(a, b));
+        System.out.println("4. Volume Bola ialah = " + lingkaran.volbola(a));
       
     }
     
