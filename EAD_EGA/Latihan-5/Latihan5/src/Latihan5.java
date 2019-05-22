815b187063e67569503c022f3f1fe48998bdf11a
Muhammad Ega S <simatupang.ega@gmail.com>
2019-05-22 15:13:52 +0700

    Update Latihan5.java

--- EAD_EGA/Latihan-5/Latihan5/src/Latihan5.java
+++ EAD_EGA/Latihan-5/Latihan5/src/Latihan5.java
@@ -4,33 +4,23 @@ public class Latihan5 {
 
     public static void main(String[] args) {
         PopulateObjek lingkaran = new PopulateObjek();
+        PopulateObjek tabung = new PopulateObjek();
+        PopulateObjek kerucut = new PopulateObjek();
+        PopulateObjek bola = new PopulateObjek();
+
+        Scanner dia = new Scanner(System.in);
+        System.out.println("Diameter (dalam cm): ");
+        int diaInt = dia.nextInt();
+        Scanner tin = new Scanner(System.in);
+        System.out.println("Tinggi(dalam cm): ");
+        int tinInt = tin.nextInt();
+
         lingkaran.hitungLuasLingkaran(10);
         System.out.println(lingkaran.luasLingkaran(20));
 
-        PopulateObjek tabung = new PopulateObjek();
-        Scanner dia = new Scanner(System.in);
-        System.out.println("Diameter tabung (dalam cm): ");
-        int diaInt = dia.nextInt();
-        Scanner tin = new Scanner(System.in);
-        System.out.println("Tinggi tabung (dalam cm): ");
-        int tinInt = tin.nextInt();
         System.out.println("Volume tabung : " + tabung.volumeTabung(diaInt, tinInt));
-
-        PopulateObjek kerucut = new PopulateObjek();
-        Scanner diameter = new Scanner(System.in);
-        System.out.println("Diameter kerucut (dalam cm): ");
-        int diameterInt = diameter.nextInt();
-        Scanner tinggi = new Scanner(System.in);
-        System.out.println("Tinggi kerucut (dalam cm): ");
-        int tinggiInt = tinggi.nextInt();
-        System.out.println("Volume kerucut : " + kerucut.volumeKerucut(diameterInt, tinggiInt));
-
-        PopulateObjek bola = new PopulateObjek();
-        Scanner d = new Scanner(System.in);
-        System.out.println("Diameter bola (dalam cm) ");
-        int dInt = d.nextInt();
-        System.out.println(bola.volumeBola(dInt));
+        System.out.println("Volume kerucut : " + kerucut.volumeKerucut(diaInt, tinInt));
+        bola.volumeBola(diaInt,tinInt);
 
     }
-
 }
