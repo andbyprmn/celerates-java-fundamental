7cab6aeae1b01cea47f35bb7f53f549dee78298e
mirfangg <muhammadirvan91@gmail.com>
2019-05-23 15:55:55 +0700

    added latihan-7

---
+++ EAD_IRFAN/latihan-7/instacelerates/model/Story.java
@@ -0,0 +1,33 @@
+package com.instacelerates.model;
+
+public class Story {
+    
+    private String foto;
+    private String tgl;
+    private String caption;
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getTgl() {
+        return tgl;
+    }
+
+    public void setTgl(String tgl) {
+        this.tgl = tgl;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+    
+}
