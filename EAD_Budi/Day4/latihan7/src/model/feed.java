3dc14a88ec4b6ce97f345c239c650c01e08f0d9c
Your Name <budi.juarto@gmail.com>
2019-05-23 15:56:25 +0700

    akhirnya pid input integer ternyata harus ditaruh di akhir

---
+++ EAD_Budi/Day4/latihan7/src/model/feed.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ACER
+ */
+public class feed {
+    private String fotoFeed;
+    private String captionFeed;
+    private int likeFeed;
+    private String commentsFeed;
+
+    public String getFotoFeed() {
+        return fotoFeed;
+    }
+
+    public void setFotoFeed(String fotoFeed) {
+        this.fotoFeed = fotoFeed;
+    }
+
+    public String getCaptionFeed() {
+        return captionFeed;
+    }
+
+    public void setCaptionFeed(String captionFeed) {
+        this.captionFeed = captionFeed;
+    }
+
+    public int getLikeFeed() {
+        return likeFeed;
+    }
+
+    public void setLikeFeed(int likeFeed) {
+        this.likeFeed = likeFeed;
+    }
+
+    public String getCommentsFeed() {
+        return commentsFeed;
+    }
+
+    public void setCommentsFeed(String commentsFeed) {
+        this.commentsFeed = commentsFeed;
+    }
+    
+
+
+   
+}
