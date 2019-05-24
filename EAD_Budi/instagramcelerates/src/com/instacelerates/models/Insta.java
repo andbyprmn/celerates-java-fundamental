e0936bdf8b831bab08e65aa6d2bc1072f34e4a79
Your Name <budi.juarto@gmail.com>
2019-05-24 14:45:38 +0700

    DAy-5

---
+++ EAD_Budi/instagramcelerates/src/com/instacelerates/models/Insta.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.instacelerates.models;
+
+/**
+ *
+ * @author ACER
+ */
+public class Insta {
+    private String foto;
+    private String Caption;
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getCaption() {
+        return Caption;
+    }
+
+    public void setCaption(String Caption) {
+        this.Caption = Caption;
+    }
+}
---
+++ EAD_Budi/latihan 8/src/polymorphismmain/PolymorphismMain.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymorphismmain;
+
+import com.celerates.polymorphism.Avengers;
+import com.celerates.polymorphism.Informasi;
+import com.celerates.polymorphism.Movie;
+
+/**
+ *
+ * @author ACER
+ */
+public class PolymorphismMain {
+    public static void main (String[] args){
+       //inheritance
+        Avengers objAvengers = new Avengers();
+        //polymorphism
+        Movie objMovie = new Avengers();
+        objMovie.negara();
+        
+        
+        Movie objMovieInfo = new Movie();
+        Informasi objInfo = new Informasi();
+        objInfo.info(objMovieInfo);
+      
+        
+    }
+}
