62af7870aa806637effb7d10cf5e8dcaca0c061e
lisafitasari <fitasarilisa13@gmail.com>
2019-05-24 14:37:17 +0700

    latihan8

---
+++ BIGDATA_Lisa/CeleratesJavaFundamental/src/com/instacelerates/model/insta.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.instacelerates.model;
+
+/**
+ *
+ * @author Lisa Fita Sari
+ */
+public class insta {
+    public String foto;
+    public String caption;
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+    
+    
+}
